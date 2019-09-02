23b58a356f3df3b265b7388329a4c5e0e93afe1a
m019m1 <50258878+m019m1@users.noreply.github.com>
2019-09-02 23:52:28 +0700

    added edit ability

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let counter = 0;
 
+// set focus to the last character in textarea
+let text = $('.newTask').val();
+$('.newTask').focus().val('').val(text);
 $('.newTask').on('keyup', function(e) {
 	
 	if( !(e.key == 'Enter' && e.ctrlKey) ) return;
@@ -7,8 +10,7 @@ $('.newTask').on('keyup', function(e) {
 	counter++;
 	
 	let $task = $('<li/>', {
-		'class': 'task',
-		text: this.value
+		'class': 'task'
 	}).appendTo('.taskList');
 
 	let $done = $("<input>", {
@@ -20,11 +22,15 @@ $('.newTask').on('keyup', function(e) {
 		'class': 'close',
 		text: 'x'
 	}).appendTo($task);
+
+	let $text = $("<p/>", {
+		text: this.value
+	}).appendTo($task);
 	
 	$done.on('click', () => {
-		if($task.is('.made')) {
-			counter++;
-		} else counter--;
+		if(!$task.is('.made')) {
+			counter--;
+		} else counter++;
 		showCount();
 		$task.toggleClass('made');
 		$close.toggle();
@@ -33,25 +39,37 @@ $('.newTask').on('keyup', function(e) {
 	$close.on('click', () => {
 		$task.remove();
 	});
+	
+	$task.on('dblclick', function edit() {
+		$task.hide();
+		let $edit = $("<textarea/>", {
+			'class': 'edit'
+		});
+		$edit.insertAfter($task);
+		$edit.focus().val( $text.text() );
+		
+		$edit.on('keyup', function(e) {
+			if( !(e.key == 'Enter' && e.ctrlKey) ) return;
+			$text.text($edit.val());
+			$edit.hide();
+			$task.show();
+		});
 
-	$task.on('mousemove', () => {
-		return false;
 	});
-
 	showCount();
 	this.value = '';
 });
 
 function showCount() {
-switch (counter) {
-	case 0:
-		$('.taskListHeader').text(`Nothing to do... boring...`);
-		break;
-	case 1:
-		$('.taskListHeader').text(`To do 1 task`);
-		break;
-	default:
-		$('.taskListHeader').text(`To do ${counter} tasks`);
-		break;
-}
+	switch (counter) {
+		case 0:
+			$('.taskListHeader').text(`Nothing to do... boring...`);
+			break;
+		case 1:
+			$('.taskListHeader').html(`To do 1 task <br> Double click to edit, "Ctrl+Enter" to save.`);
+			break;
+		default:
+			$('.taskListHeader').html(`To do ${counter} tasks <br> Double click to edit, "Ctrl+Enter" to save.`);
+			break;
+	}
 }
